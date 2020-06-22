a946116e05800ca32fca5c3c4e989ca26fd5b357
Michal Ďuračík <Michal.Duracik@fri.uniza.sk>
2020-06-22 10:52:24 +0200

    Created sample app

---
+++ Echo.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Echo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Echo {
+    
+    public static void main() {
+    }
+    
+}
